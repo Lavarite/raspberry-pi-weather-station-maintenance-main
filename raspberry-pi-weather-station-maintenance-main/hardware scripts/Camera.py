69b7882915d4db2235bd8d976b124e7f41ab4e3a
Lavarite <102068981+Lavarite@users.noreply.github.com>
2023-03-03 14:07:05 +0000

    Update Camera.py

--- raspberry-pi-weather-station-maintenance-main/hardware scripts/Camera.py
+++ raspberry-pi-weather-station-maintenance-main/hardware scripts/Camera.py
@@ -1,11 +1,8 @@
 from picamera import PiCamera
 from time import sleep
 
-
-def main():
-    SERVER_URL = 'http://172.20.47.242:80/data'
-    camera = PiCamera()
-    camera.start_preview()
-    sleep(5)
-    camera.capture('/tmp/picture.jpg')
-    camera.stop_preview()
+camera = PiCamera()
+camera.start_preview()
+sleep(5)
+camera.capture('/tmp/view.jpg')
+camera.stop_preview()
